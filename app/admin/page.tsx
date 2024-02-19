9b4a8b076a0506f8c3e43f7cbe75954a18538a83
Yvan Yang <ybyang@best-inc.com>
2024-02-19 16:25:49 +0800

    update domain

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,23 +1,23 @@
-import { auth } from 'app/auth';
-import { getGuestbookEntries } from 'app/db/queries';
-import { redirect } from 'next/navigation';
-import Form from './form';
+import { auth } from "app/auth";
+import { getGuestbookEntries } from "app/db/queries";
+import { redirect } from "next/navigation";
+import Form from "./form";
 
 export const metadata = {
-  title: 'Admin',
+  title: "Admin",
 };
 
 export default async function GuestbookPage() {
   let session = await auth();
-  if (session?.user?.email !== 'me@leerob.io') {
-    redirect('/');
+  if (session?.user?.email !== "me@studycraft.dev") {
+    redirect("/");
   }
 
   let entries = await getGuestbookEntries();
 
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">admin</h1>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">admin</h1>
       <Form entries={entries} />
     </section>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import './global.css';
-import type { Metadata } from 'next';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-import { Navbar } from './components/nav';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { SandpackCSS } from './blog/[slug]/sandpack';
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SandpackCSS } from "./blog/[slug]/sandpack";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.io'),
+  metadataBase: new URL("https://studycraft.dev"),
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: "Study craft",
+    template: "%s | Study craft",
   },
-  description: 'Developer, writer, and creator.',
+  description: "Developer, writer, and creator.",
   openGraph: {
-    title: 'Lee Robinson',
-    description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
-    locale: 'en_US',
-    type: 'website',
+    title: "Study craft",
+    description: "Developer, writer, and creator.",
+    url: "https://studycraft.dev",
+    siteName: "Study craft",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -28,22 +28,22 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   twitter: {
-    title: 'Lee Robinson',
-    card: 'summary_large_image',
+    title: "Study craft",
+    card: "summary_large_image",
   },
   verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
+    google: "TODO",
+    yandex: "TODO",
   },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(' ');
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
@@ -54,16 +54,16 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        "bg-white text-black dark:bg-[#111010] dark:text-white",
         GeistSans.variable,
-        GeistMono.variable
+        GeistMono.variable,
       )}
     >
       <head>
         <SandpackCSS />
       </head>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto">
+        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
           <Navbar />
           {children}
           <Analytics />
